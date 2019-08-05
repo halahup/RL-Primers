fixing the bug with the discounted returns calculation

Actor-Critic/a2c_cartpole.py
@@ -10,15 +10,15 @@ from torch.nn.utils import clip_grad_value_
 from collections import deque
 
 
-ALPHA = 0.0001              # learning rate for the actor
-BETA = 0.0001               # learning rate for the critic
+ALPHA = 0.001              # learning rate for the actor
+BETA = 0.001               # learning rate for the critic
 GAMMA = 0.99               # discount rate
 HIDDEN_SIZE = 128          # number of hidden nodes we have in our approximation
 PSI = 0.1                  # the entropy bonus multiplier
 
 NUM_EPISODES = 25
 NUM_EPOCHS = 5000
-NUM_STEPS = 7
+NUM_STEPS = 5
 
 RENDER_EVERY = 100
 
@@ -73,8 +73,8 @@ def get_discounted_returns(rewards: np.array, gamma: float, state_values: torch.
             discounted_rewards: the sequence of the discounted returns from time step t
     """
     discounted_rewards = np.empty_like(rewards, dtype=np.float)
-    gamma_array = np.full(shape=(n,), fill_value=gamma)
-    power_gamma_array = np.power(gamma_array, np.arange(n))
+    gamma_array = np.full(shape=(n+1,), fill_value=gamma) if n != 1 else None
+    power_gamma_array = np.power(gamma_array, np.arange(n+1)) if n != 1 else None
 
     # turn the state values torch tensor into the numpy array
     state_values = state_values.numpy()
@@ -85,11 +85,23 @@ def get_discounted_returns(rewards: np.array, gamma: float, state_values: torch.
     # for every time step in the sequence
     for t in range(T):
 
+        # special case of 1 step lookahead bootstrapping
+        if n == 1:
+
+            # check if we can discount
+            if t < T - 1:
+                Gt = rewards[t] + state_values[t+1]
+
+            else:
+
+                # the last reward
+                Gt = rewards[T-1]
+
         # check if we can bootstrap
-        if t+n < T:
+        elif t+n < T:
 
             # calculate the bootstrapped return
-            Gt = np.sum(power_gamma_array[:-1] * rewards[t:(t+n)-1]) + power_gamma_array[-1] * state_values[t+n]
+            Gt = np.sum(power_gamma_array[:-1] * rewards[t:(t+n)]) + power_gamma_array[-1] * state_values[t+n]
 
         # if we can't bootstrap anymore
         else:
@@ -231,8 +243,8 @@ def main():
     # run for N epochs
     for epoch in range(NUM_EPOCHS):
 
-        if epoch % RENDER_EVERY == 0:
-            env.render()
+        # if epoch % RENDER_EVERY == 0:
+        #     env.render()
 
         # holder for the weighted log-probs
         epoch_weighted_log_probs = torch.empty(size=(0,), dtype=torch.float)